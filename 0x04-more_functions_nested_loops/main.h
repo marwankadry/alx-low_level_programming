5de8dffc2e0478f6b4bd22e3ebb2e1fecc390ec6
marwankadry <marwankadry8@gmail.com>
2023-06-22 15:36:15 -0700

    upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-int -putchar(char c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void pinrt_most_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagona1(int n);
+void print_square(int size);
+void print_traingle(int size);
 
 #endif
