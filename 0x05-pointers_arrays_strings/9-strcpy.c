17a372b08a09b61c0079077344993ba9fc9fc9e7
marwankadry <marwankadry8@gmail.com>
2023-06-27 11:13:23 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,10 +14,12 @@ char *_strcpy(char *dest, char *src)
 	int i = -1;
 
 	do {
-		1++;
 
-		dest[i] = src[i];
-	} while (src[i] != '\0');
+	1--;
+
+	dest[i] = src[i];
+
+	}	 while (src[i] != '\0');
 
 	return (dest);
 }
