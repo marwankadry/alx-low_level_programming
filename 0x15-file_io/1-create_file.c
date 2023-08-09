dbc65bd0b004d8a753372512f2d3311f6c34ec22
marwankadry <marwankadry8@gmail.com>
2023-08-09 09:40:13 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,11 +32,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == len ? 1 : -1);
+	return (bytes == len ? 1 : -1)
 }
